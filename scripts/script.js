87e9a61b14631be2275ed3c637fe8328a6d0284c
AmmarEldik <vektorea60@gmail.com>
2022-11-08 17:43:15 +0200

    Ammar edet audio

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,8 @@ let str = document.querySelector(".str");
 str.addEventListener("click",function cut() {
     document.getElementById("start").style.display = "none";
     document.getElementById("loding").style.display = "block";
+    document.getElementById("audio-loding").play();
     setTimeout(()=> document.getElementById("loding").style.display = "none" ,5500)
+
 })
 
